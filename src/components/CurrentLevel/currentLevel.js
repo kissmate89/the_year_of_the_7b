5170f7c479361f9e498614f381b1fb65ab58c3c2
Mate Kiss <kissmate89@gmail.com>
2020-02-27 20:15:38 +0100

    Get grade from api

--- src/components/CurrentLevel/currentLevel.js
+++ src/components/CurrentLevel/currentLevel.js
@@ -5,18 +5,18 @@ import ProgressBar from "../ProgressBar/progressBar"
 import { H3 } from "../../styles/component.styles"
 import { CurrentLevelWrapperStyled } from "./currentLevel.styles"
 
-const CurrentLevel = ({}) => {
+const CurrentLevel = ({ boulderGrades, leadGrades }) => {
   return (
     <CurrentLevelWrapperStyled>
       <H3>Boulder</H3>
       <ProgressBar
-        grades={["6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b"]}
-        currentGrade="6c+"
+        grades={boulderGrades.grades}
+        currentGrade={boulderGrades.currentGrade}
       />
       <H3>Lead</H3>
       <ProgressBar
-        grades={["6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b"]}
-        currentGrade="6c"
+        grades={leadGrades.grades}
+        currentGrade={leadGrades.currentGrade}
       />
     </CurrentLevelWrapperStyled>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,19 +18,31 @@ const IndexPage = () => (
             content {
               content
             }
+            leadGrades {
+              currentGrade
+              grades
+            }
+            boulderGrades {
+              currentGrade
+              grades
+            }
           }
         }
       }
     `}
     render={data => {
       const mainData = data.allContentfulClimbingMainPage.nodes[0]
+
       return (
         <Layout>
           <SEO title={mainData.title} />
           <MainTextStyled>{mainData.content.content}</MainTextStyled>
           <TitleStyled>Current level</TitleStyled>
-          <CurrentLevel />
-          <TitleStyled>Recent posts</TitleStyled>
+          <CurrentLevel
+            boulderGrades={mainData.boulderGrades}
+            leadGrades={mainData.leadGrades}
+          />
+          <TitleStyled>Posts</TitleStyled>
           <PostList />
         </Layout>
       )
