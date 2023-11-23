04b8011f0c6802ecda2322fcf3539c019861527c
Mate Kiss <kissmate89@gmail.com>
2023-11-23 17:29:50 +0100

    Fix failing test because of syntax changes

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,14 @@ module.exports = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testURL: `http://localhost`,
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: `http://localhost`,
+
+  },
   setupFiles: [`<rootDir>/loadershim.js`],
-  setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
 }
--- src/utils/__tests__/darkLight.provider.test.js
+++ src/utils/__tests__/darkLight.provider.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import '@testing-library/jest-dom'
 
 import testingRenderer from "../testingRenderer";
 import { useDarkLightProvider } from "../darkLight.provider";
