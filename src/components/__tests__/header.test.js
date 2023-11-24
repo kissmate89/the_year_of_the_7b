c8bfa3e0b3e05f35721be9965d33cd5962c095ca
Mate Kiss <kissmate89@gmail.com>
2023-11-24 15:37:26 +0100

    Fix test for header

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -1,32 +1,20 @@
 import React from "react";
 import { fireEvent } from "@testing-library/react";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
 import testingRenderer from "../../utils/testingRenderer";
 
 import Header from "../header";
 
 describe("Header", () => {
-  it("renders the title correctly", () => {
-    const { getByTestId } = testingRenderer(
-      <Header siteTitle="The year of the 7b" />
-    );
-
-    expect(getByTestId("header-title")).toHaveTextContent("The year of the 7b");
-  });
-
   it("renders the moon icon when light theme is on", () => {
-    const { getByTestId } = testingRenderer(
-      <Header siteTitle="The year of the 7b" />
-    );
+    const { getByTestId } = testingRenderer(<Header />);
 
     expect(getByTestId("moon-icon")).toBeTruthy();
   });
 
   it("renders the sun icon when dark theme is on", () => {
-    const { getByTestId, getByLabelText } = testingRenderer(
-      <Header siteTitle="The year of the 7b" />
-    );
+    const { getByTestId, getByLabelText } = testingRenderer(<Header />);
 
     fireEvent.click(getByLabelText("theme switch"));
 
