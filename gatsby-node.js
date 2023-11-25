c3641adf7b5690b9b02831d7c2bacb0f266c8ef6
Máté Kiss <kissmate89@gmail.com>
2023-11-25 20:17:16 +0100

    Implement Strava SSR rendering (#33)
    * Add SSR activities page
    
    * Move not used files for safekeeping

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-const path = require("path")
-const { slash } = require("gatsby-core-utils")
+const path = require("path");
+const { slash } = require("gatsby-core-utils");
 
 // Implement the Gatsby API “createPages”. This is
 // called after the Gatsby bootstrap is finished so you have
@@ -10,12 +10,12 @@ exports.createPages = async ({ graphql, actions }) => {
   // queries against the local Contentful graphql schema. Think of
   // it like the site has a built-in database constructed
   // from the fetched data that you can run queries against.
-  const { createPage } = actions
-  const postTemplate = path.resolve(`./src/templates/Post/post.js`)
+  const { createPage } = actions;
+  const postTemplate = path.resolve(`./src/templates/Post/post.js`);
 
-  const results = await graphql(
-    `{
-      allContentfulClimbingPosts(sort: {createdAt: DESC}) {
+  const results = await graphql(`
+    {
+      allContentfulClimbingPosts(sort: { createdAt: DESC }) {
         edges {
           node {
             slug
@@ -41,11 +41,11 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
-    }`
-  )
+    }
+  `);
 
   results.data.allContentfulClimbingPosts.edges.forEach((result) => {
-    const { node, next, previous } = result
+    const { node, next, previous } = result;
 
     // Gatsby uses Redux to manage its internal state.
     // Plugins and sites can use functions like "createPage"
@@ -65,6 +65,36 @@ exports.createPages = async ({ graphql, actions }) => {
         next: next,
         previous: previous,
       },
-    })
+    });
+  });
+
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${process.env.STRAVA_CLIENT_ID}&client_secret=${process.env.STRAVA_CLIENT_SECRET}&refresh_token=${process.env.STRAVA_REFRESH_TOKEN}&grant_type=refresh_token`;
+  const callActivities = `https://www.strava.com/api/v3/athlete/activities?per_page=10&access_token=`;
+  const activityPage = path.resolve(`./src/templates/activities.js`);
+  const getActivities = (access) =>
+    fetch(callActivities + access)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((e) => console.log(e));
+
+  const stravaResult = await fetch(callRefresh, {
+    method: "POST",
   })
-}
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      return getActivities(result.access_token);
+    });
+
+  createPage({
+    // Each page is required to have a `path` as well
+    // as a template component. The `context` is
+    // optional but is often necessary so the template
+    // can query data specific to each page.
+    path: `/activities`,
+    component: slash(activityPage),
+    context: {
+      activities: stravaResult,
+    },
+  });
+};
